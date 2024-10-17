01c3d4e6684b18b8fb5ee3be20cfcd6819cb8512
“dbdoan” <40965142+dbdoan@users.noreply.github.com>
2024-10-17 02:33:10 -0500

    test
    - example change 1
    - example change 2
    - example change 3

--- github_updater_cog.py
+++ github_updater_cog.py
@@ -38,11 +38,11 @@ class GitHubUpdater(commands.Cog):
                     f"✅ **Commit has been pushed to Main** ✅\n\n"
                     f"**User**: {user}\n\n"
                     f"**Updates**: \n{commit_message}\n"
-                    "# /////////// /////////// /////////// #\n"
+                    "////////// /////////// ///////////\n"
                     "\n"
                     f"Github Update: {commit_url}\n"
                     "\n"
-                    "Quick development view: https://trello.com/invite/b/6710b55f4a2754602f1f967a/ATTI88d9ffb5fda2422e82465fd5f204991635C3EF9C/atelier-404"
+                    "Quick development view: <https://trello.com/invite/b/6710b55f4a2754602f1f967a/ATTI88d9ffb5fda2422e82465fd5f204991635C3EF9C/atelier-404>"
                 )
 
                 await channel.send(message)
